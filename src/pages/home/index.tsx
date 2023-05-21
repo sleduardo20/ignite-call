f73241b4c96ad97f3b927e5ae0ce038a824585ce
sleduardo20 <sl.eduardo20@gmail.com>
2023-05-20 21:49:20 -0300

    🧯 fix file extention

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,18 +8,23 @@ export default function Home() {
   return (
     <Container>
       <Hero>
-        <Heading size="4xl">
-          Agendamento descomplicado
-        </Heading>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
 
         <Text size="lg">
-          Conecte seu calendario e permita que as pessoas marquem agendamentos no seu tempo livre.
+          Conecte seu calendario e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
         </Text>
 
         <ClaimUsernameForm />
       </Hero>
       <Preview>
-        <Image src={appPreview} quality={100} priority height={400} alt="Preview image" />
+        <Image
+          src={appPreview}
+          quality={100}
+          priority
+          height={400}
+          alt="Preview image"
+        />
       </Preview>
     </Container>
   );
