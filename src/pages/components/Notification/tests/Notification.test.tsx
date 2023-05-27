f225acbae42e7a6463145ac53e0fa93080c053ed
sleduardo20 <sl.eduardo20@gmail.com>
2023-05-27 16:12:57 -0300

    ✔ refactor test for Notification component

--- src/pages/components/Notification/tests/Notification.test.tsx
+++ src/pages/components/Notification/tests/Notification.test.tsx
@@ -1,19 +1,9 @@
 import { toast } from 'react-toastify';
 import { notification } from '..';
 
-jest.mock('react-toastify', () => ({
-  toast: {
-    error: jest.fn(),
-    success: jest.fn(),
-    info: jest.fn(),
-  },
-}));
+jest.mock('react-toastify');
 
 describe('Notification Component', () => {
-  const toastErrorMock = toast.error as jest.Mock;
-  const toastSuccessMock = toast.success as jest.Mock;
-  const toastInfoMock = toast.info as jest.Mock;
-
   afterEach(() => {
     jest.clearAllMocks();
   });
@@ -21,16 +11,13 @@ describe('Notification Component', () => {
   it('should call toast.error with the correct message', () => {
     const errorMessage = 'Something went wrong';
     notification.error(errorMessage);
-    expect(toastErrorMock).toHaveBeenCalledWith(
-      errorMessage,
-      expect.any(Object),
-    );
+    expect(toast.error).toHaveBeenCalledWith(errorMessage, expect.any(Object));
   });
 
   it('should call toast.success with the correct message', () => {
     const successMessage = 'Action completed successfully';
     notification.success(successMessage);
-    expect(toastSuccessMock).toHaveBeenCalledWith(
+    expect(toast.success).toHaveBeenCalledWith(
       successMessage,
       expect.any(Object),
     );
@@ -39,6 +26,6 @@ describe('Notification Component', () => {
   it('should call toast.info with the correct message', () => {
     const infoMessage = 'New notification';
     notification.notify(infoMessage);
-    expect(toastInfoMock).toHaveBeenCalledWith(infoMessage, expect.any(Object));
+    expect(toast.info).toHaveBeenCalledWith(infoMessage, expect.any(Object));
   });
 });
